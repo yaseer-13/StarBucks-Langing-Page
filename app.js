548fcf900cdfb3511e6f186f69c39368fe798208
Yaseer Tasleem <yaseertasleem@Local-Air.local>
2021-12-26 14:21:27 +0530

    Uploading the starbucks Landing Page code

---
+++ app.js
@@ -0,0 +1,28 @@
+function imgSlider(anything) {
+    document.querySelector('.starbucks').src = anything;
+}
+
+function changeCircleColor(color) {
+    const circle = document.querySelector('.circle');
+    const button = document.querySelector('.button');
+    const Home = document.querySelector('.Home');
+    const Menu = document.querySelector('.Menu');
+    const New = document.querySelector('.New');
+    const Contact = document.querySelector('.Contact');
+    const textStarbucks = document.querySelector('.textStarbucks');
+
+    circle.style.background = color;
+    button.style.background = color;
+    textStarbucks.style.color = color;
+    Home.style.color = color;
+    Menu.style.color = color;
+    New.style.color = color;
+    Contact.style.color = color;
+}
+
+function toggleMenu() {
+    var menuToggle = document.querySelector('.toggle');
+    var navigation = document.querySelector('.navigation');
+    menuToggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+}
